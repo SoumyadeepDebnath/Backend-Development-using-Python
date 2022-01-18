1b452eb26ede699bf4d39255c1885002f6e1493b
Soumyadeep Debnath <soumyadebnath13@gmail.com>
2022-01-18 22:09:11 +0530

    Update app.py

--- simple_flask_project/app.py
+++ simple_flask_project/app.py
@@ -3,15 +3,16 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 
+
 @app.route('/', methods=['GET', 'POST']) # To render Homepage
 def home_page():
     return render_template('index.html')
 
-@app.route('/math', methods=['POST'])  # This will be called from UI
+@app.route('/via_webpage', methods=['POST'])        # calling the API from from UI
 def math_operation():
     if (request.method=='POST'):
-        operation=request.form['operation']
-        num1=int(request.form['num1'])
+        operation = request.form['operation']
+        num1 = int(request.form['num1'])
         num2 = int(request.form['num2'])
         if(operation=='add'):
             r=num1+num2
@@ -27,7 +28,7 @@ def math_operation():
             result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
         return render_template('results.html',result=result)
 
-@app.route('/via_postman', methods=['POST']) # for calling the API from Postman/SOAPUI
+@app.route('/via_postman', methods=['POST'])        # calling the API from Postman/SOAPUI
 def math_operation_via_postman():
     if (request.method=='POST'):
         operation=request.json['operation']
@@ -46,6 +47,14 @@ def math_operation_via_postman():
             r = num1 / num2
             result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
         return jsonify(result)
+
+@app.route('/via_browser_url')    # calling the API from Browser URL
+def math_operation_via_browser():
+        #query parameters
+        num1 = request.args.get('val1')
+        num2 = request.args.get('val2')
+        return '''<h1>My Result is : {}</h1>'''.format()
+
 
 
 if __name__ == '__main__':
